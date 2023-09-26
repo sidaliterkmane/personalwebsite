d06b14d225910e3ece49e57cb81b5bf389f9c774
Sid Ali Terkmane <sidterkmane@hotmail.com>
2023-09-26 13:14:31 -0400

    cleaned assets

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from "next/image"
-import pp from '../../public/images/pp.jpg'
+import pp from '../../public/images/profilePicture.jpg'
 
 import { motion } from "framer-motion"
 
